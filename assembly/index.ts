0b28958469d4ee6af61bef205c0a96512e6f2357
johngomes-github <johngomes144@gmail.com>
2022-12-06 11:02:34 +0000

    Created Greeting conract.

---
+++ assembly/index.ts
@@ -0,0 +1,23 @@
+import {storage, context} from "near-sdk-as";
+
+export class Greeting{
+
+    getGreeting(accountId: string): string | null {
+        
+    return storage.get<string>(accountId, "Greeting not available");
+
+
+    }
+
+
+    setGreeting(greeting: string): void {
+
+        storage.set(context.sender,greeting);
+    
+
+    }
+
+
+
+}
+
